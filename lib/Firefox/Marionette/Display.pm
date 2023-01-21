7431bf63250e762708a97ffcbd9d9dd34b08df8c
David Dick <ddick@cpan.org>
2023-01-21 21:03:47 +1100

    Update copyright notice

--- lib/Firefox/Marionette/Display.pm
+++ lib/Firefox/Marionette/Display.pm
@@ -167,7 +167,7 @@ David Dick  C<< <ddick@cpan.org> >>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2022, David Dick C<< <ddick@cpan.org> >>. All rights reserved.
+Copyright (c) 2023, David Dick C<< <ddick@cpan.org> >>. All rights reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic/perlartistic>.
