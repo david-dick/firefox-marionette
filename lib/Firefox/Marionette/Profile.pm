7c1ba76c42a50f24ccaef93fe61a4acbdff73b7e
David Dick <ddick@cpan.org>
2023-08-14 20:33:55 +1000

    Allow SOCKS proxies to use remote DNS

--- lib/Firefox/Marionette/Profile.pm
+++ lib/Firefox/Marionette/Profile.pm
@@ -225,6 +225,7 @@ sub new {
     $profile->set_value( 'marionette.port',                       ANY_PORT() );
     $profile->set_value( 'network.http.prompt-temp-redirect',     'false', 0 );
     $profile->set_value( 'network.http.request.max-start-delay',  '0',     0 );
+    $profile->set_value( 'network.proxy.socks_remote_dns',        'true',  0 );
     $profile->set_value( 'security.osclientcerts.autoload',       'true',  0 );
     $profile->set_value( 'signon.autofillForms',                  'false', 0 );
     $profile->set_value( 'signon.rememberSignons',                'false', 0 );
