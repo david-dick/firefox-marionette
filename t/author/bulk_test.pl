89f350d09c929fc18dd5cbbd19093bdcaf74e787
David Dick <ddick@cpan.org>
2023-09-14 19:54:22 +1000

    Shutdown Win32 VMs as soon as possible

--- t/author/bulk_test.pl
+++ t/author/bulk_test.pl
@@ -223,6 +223,8 @@ MAIN: {
 								_execute($server, undef, 'scp', '-r', '-P', $server->{port}, $server->{user} . q[@] . $server->{address} . q[:/] . $remote_tmp_directory . q[/firefox-marionette/] . $cover_db_name, Cwd::cwd() . '/');
 							}
 							_cleanup_server($server);
+							_virsh_shutdown($server);
+							_sleep_until_shutdown($server);
 						} elsif ($server->{os} eq 'android') {
 							my $count = 0;
 							_execute($server, { alarm_after => $ENV{FIREFOX_ALARM}, return_result => 1 }, $^X, ($devel_cover_inc ? $devel_cover_inc : ()), '-Ilib', '-MFirefox::Marionette', '-e', "Firefox::Marionette->new(adb => '$server->{address}');");
